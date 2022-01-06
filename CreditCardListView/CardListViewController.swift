2ef93ad684e8776a4b18ef2d948b5a1d4a19bfa3
min-kyoung <shalsrud9@naver.com>
2022-01-06 15:28:17 +0900

    king fisher + lottie + firebase realtime database

--- CreditCardListView/CardListViewController.swift
+++ CreditCardListView/CardListViewController.swift
@@ -7,8 +7,10 @@
 
 import UIKit
 import Kingfisher
+import FirebaseDatabase
 
 class CardListViewController: UITableViewController{
+    var ref: DatabaseReference! // Firebase realtime reference를 가져올 수 있는 값
     var creditCardList: [CreditCard] = []
     
     override func viewDidLoad() {
@@ -17,6 +19,29 @@ class CardListViewController: UITableViewController{
         // UITableView Cell Register
         let nibName = UINib(nibName: "CardListCell", bundle: nil)
         tableView.register(nibName, forCellReuseIdentifier: "CardListCell")
+        
+        ref = Database.database().reference()
+        
+        // ref가 value 값을 바라보고 그 다음에는 snapshot을 이용해서 데이터를 불러옴
+        ref.observe(.value) { snapshot in
+            guard let value = snapshot.value as? [String: [String: Any]] else { return }
+            
+            // json decoding
+            do{
+                let jsonData = try JSONSerialization.data(withJSONObject: value)
+                let carData = try JSONDecoder().decode([String: CreditCard].self, from: jsonData)
+                let cardList = Array(carData.values)
+                self.creditCardList = cardList.sorted { $0.rank < $1.rank } // 순위 순서대로 보여줌
+                
+                // table view reload
+                // 메인 스레드에 해당 액션을 넣어주는 것
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+            } catch let error {
+                print("ERROR JSON parsing \(error.localizedDescription)")
+            }
+        }
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -45,11 +70,40 @@ class CardListViewController: UITableViewController{
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         // 각 셀이 표현하고 있는 카드의 혜택 상세 정보가 담긴 화면으로 이동
         let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
-        guard let detailViewController = storyboard.instantiateViewController(identifier: "CardDateViewController")
+        guard let detailViewController = storyboard.instantiateViewController(identifier: "CardDetailViewController")
                 as? CardDetailViewController else { return }
         
         detailViewController.promotionDetail = creditCardList[indexPath.row].promotionDetail
         self.show(detailViewController, sender: nil)
+        
+        // 특정 key의 값이 cardID와 같은 객체를 찾아서 snapshot으로 찍는 것
+        let cardID = creditCardList[indexPath.row].id
+        ref.queryOrdered(byChild: "id").queryEqual(toValue: cardID).observe(.value) {[weak self] snapshot in
+            guard let self = self,
+                  let value = snapshot.value as? [String: [String: Any]],
+                  let key = value.keys.first else { return }
+            
+            self.ref.child("\(key)/isSelected").setValue(true)
+        }
+    }
+    
+    // 카드 셀 삭제
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        // edigingStyle이 삭제인 경우
+        if editingStyle == .delete {
+            let cardID = creditCardList[indexPath.row].id
+            ref.queryOrdered(byChild: "id").queryEqual(toValue: cardID).observe(.value) { [weak self] snapshot in
+                guard let self = self,
+                      let value = snapshot.value as? [String: [String: Any]],
+                      let key = value.keys.first else { return }
+                
+                self.ref.child(key).removeValue()
+            }
+        }
     }
 }
 
